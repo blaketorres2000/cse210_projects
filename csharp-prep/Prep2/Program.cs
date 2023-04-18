d682e28ce60daadad922b13a904e7eb4656cd39b
blaketorres2000 <blaketorres2000@yahoo.com>
2023-04-18 16:24:05 -0500

    Week 1 Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You have a grade of {letterGrade}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Do better!");
+        }    
     }
 }
