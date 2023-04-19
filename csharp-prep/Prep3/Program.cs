0e6efbb9e8e92cf3b5bf1f8686e9fc69d686a25f
blaketorres2000 <blaketorres2000@yahoo.com>
2023-04-18 20:25:01 -0500

    Week 2 Prep3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess a number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
