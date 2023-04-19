2b7042e6f3b806821b8cab5c78da608eb2237b8e
blaketorres2000 <blaketorres2000@yahoo.com>
2023-04-19 14:54:48 -0500

    Week 2 Prep5 Assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string Name = Console.ReadLine();
+            return Name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            string number = Console.ReadLine();
+            int num = int.Parse(number);
+            return num;
+        }
+        static int SquareNumber(int numberInput)
+        {
+            int SquareRoot = (int)Math.Sqrt(numberInput);
+            return SquareRoot;
+        }
+        DisplayWelcome();
+        string Namer = PromptUserName();
+        int number = PromptUserNumber();
+        Console.WriteLine($"{Namer}, the square root of your number is {SquareNumber(number)}");
     }
 }
